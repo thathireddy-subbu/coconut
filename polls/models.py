6429bb905128b203c209a937ca48a15db0c5dd82
Subbu <thathireddysubbu1996@gmail.com>
2023-03-18 11:19:06 +0530

    second module

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class student(models.Model):
+    name = models.CharField(max_length=40)
+    age = models.IntegerField()
+    marks = models.IntegerField()
+    college_name = models.CharField(max_length=50)
+    city = models.CharField(max_length=40)
+    def __str__(self):
+        return self.name
